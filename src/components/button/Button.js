a8ea7c851a338f9ef1d18e32a97ade44d0551fce
amarjeetrao <amarjeetrao33@gmail.com>
2020-12-30 11:33:50 +0530

    Progress Bar with Button Active

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './style.css'
 
-function Button({type, size, handleClick, children}) {
+function Button({type, size, handleClick, loading, children}) {
 	const className = `btn ${type} ${size ? size : ''}`;
 
 	return (
-		<button className={className} onClick={handleClick}>
+		<button className={className} onClick={handleClick} disabled={loading && loading}>
 			{children}
 		</button>
 	);
--- src/containers/controlledProgressBar/ControlledProgressBar.js
+++ src/containers/controlledProgressBar/ControlledProgressBar.js
@@ -6,19 +6,63 @@ import Button from '../../components/button/Button'
 import Style from './style.module.css'
 
 export default class ControlledProgressBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.timer = 0;
+		this.IntervalSpeed = 100;
+
+		this.state = {
+			loading: false,
+			progress: 0
+		};
+	}
+
+	startProgress = () => {
+
+		this.timer = setInterval(() => {
+			this.setState(prevState => ({
+				loading: true,
+				progress: prevState.progress + 1
+			}));
+		}, this.IntervalSpeed);
+
+	}
+
+	pauseProgress = () => {
+		clearInterval(this.timer);
+		this.setState({ loading: false })
+
+		if (this.state.progress === 100) {
+			this.setState({ loading: false, progress: 0 })
+		}
+	}
+
+	componentDidUpdate(prevProp, prevState) {
+		if (prevState.progress >= 99) {
+			clearInterval(this.timer);
+		}
+
+	}
+
 	render() {
 		return (
 			<div className={Style.ControlledProgressBar}>
 				<ProgressBar
-					progress={20}
+					progress={this.state.progress}
 					size={235}
 					strokeWidth={12}
 					strokeStyle='round'
 					circleBgStroke='white'
 					circleStroke='#0ebffd'
 				/>
-				<Button type='primary'>Start</Button>
-				<Button>Pause</Button>
+				<Button
+					type='primary'
+					handleClick={this.startProgress}
+					loading={this.state.loading}>
+					Start
+				</Button>
+				<Button handleClick={this.pauseProgress}>{this.state.progress === 100 ? 'Reset' : 'Pause'}</Button>
 			</div>
 		);
 	}
